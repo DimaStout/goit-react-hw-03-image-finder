f01ec43c10b52cfaaa130a4c95411f5dcacc649c
Dmytro Zinchuk <127327533+DimaStout@users.noreply.github.com>
2023-11-12 16:39:13 +0100

    hw-03-image-finder-003

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,87 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import toast, { Toaster } from 'react-hot-toast';
+import { searchByName } from './Helper/api-request';
+
+export class App extends Component {
+  state = {
+    searchItem: '',
+    images: [],
+    page: 1,
+    perPage: 12,
+    isLoad: false,
+    error: false,
+    availablePages: 0,
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { searchItem, page, perPage, availablePages } = this.state;
+
+    if (prevState.searchItem !== searchItem || prevState.page !== page) {
+      try {
+        this.setState({ isLoad: true, error: false });
+        const cleanName = searchItem.split('/').pop();
+        const imagesData = await searchByName(cleanName, page);
+        this.setState(prevState => {
+          return {
+            images: [...prevState.images, ...imagesData.hits],
+            availablePages: Math.ceil(imagesData.totalHits / perPage),
+          };
+        });
+      } catch (error) {
+        this.setState({ error: true });
+        if (availablePages === 0) {
+          toast.error(
+            'Nothing found. Check the correctness of the search word.'
+          );
+        }
+      } finally {
+        this.setState({ isLoad: false });
+      }
+    }
+  }
+
+  onSubmitSearch = e => {
+    e.preventDefault();
+    const searchName = e.target.elements.itemToSearch.value;
+    this.setState({
+      searchItem: `${Date.now()}/${searchName}`,
+      page: 1,
+      images: [],
+    });
+  };
+
+  onLoadMore = () => {
+    this.setState(prevState => {
+      return { page: prevState.page + 1 };
+    });
+  };
+
+  render() {
+    const containerStyle = {
+      display: 'grid',
+      gridTemplateColumns: '1fr',
+      gridGap: '16px',
+      paddingBottom: '24px',
+    };
+
+    const { isLoad, images, error, page, availablePages } = this.state;
+
+    return (
+      <div style={containerStyle}>
+        <Searchbar onSubmit={this.onSubmitSearch} />
+        {error && (
+          <b>Oops! Something went wrong! Please try reloading this page! 🥹</b>
+        )}
+        {isLoad ? <Loader /> : <ImageGallery items={images} />}
+        {page !== availablePages && images.length !== 0 && (
+          <Button onClick={this.onLoadMore} />
+        )}
+        <Toaster />
+      </div>
+    );
+  }
+}
